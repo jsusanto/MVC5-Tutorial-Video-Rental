a8682cb38ce187754e2df37921ad4b9eaefbfbbf
Jeffry Susanto <jeffry.susanto@hotmail.com>
2019-07-17 11:58:19 +1000

    Initial commit and pass parameter using common model

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModel;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek" };
+
+            /*
+             * Don't use the ViewData or ViewBag to pass data to the View
+            ViewData["RandomMovie"] = movie;
+            ViewBag.RandomMovie = movie;
+            */
+
+            return View(movie);
+            //return Content("Hellow World");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("Id = " + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("PageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content("Year = " + year + " / Month = " + month);
+        }
+    }
+}
