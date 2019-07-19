190ddc7fc7e7c4a59dfb45d4ce92d183e2b5dabe
Jeffry Susanto <jeffry.susanto@hotmail.com>
2019-07-19 10:38:39 +1000

    Load customers from Database

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -9,17 +9,37 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            //It's a disposable object, so we need to dispose this object properly
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Customers
         public ActionResult Index()
         {
-            var customers = GetCustomers();
+
+            //Populate Manual Customer Records
+            //var customers = GetCustomers();
+
+            var customers = _context.Customers.ToList();
 
             return View(customers);
         }
 
         public ActionResult Details(int Id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == Id);
+            //Source from the manual populated data
+            //var customer = GetCustomers().SingleOrDefault(c => c.Id == Id);
+
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == Id);
 
             if (customer == null)
                 return HttpNotFound();
