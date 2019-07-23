214c2aab704ecc33a6c5a6006d52a5835c67cdf7
Jeffry Susanto <jeffry.susanto@hotmail.com>
2019-07-24 09:38:55 +1000

    Validation Summary on View

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -124,6 +124,7 @@ namespace Vidly.Controllers
 
             var viewModel = new CustomerFormViewModel
             {
+                Customer = new Customer(),
                 MembershipTypes = membershipTypes
             };
 
