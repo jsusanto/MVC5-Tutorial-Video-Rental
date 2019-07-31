da6ae2db59605d67a5b1d03c64e1effada6ef259
Jeffry Susanto <jeffry.susanto@hotmail.com>
2019-07-31 11:41:54 +1000

    enable or disable cache on a page (controller)

--- Vidly/Controllers/HomeController.cs
+++ Vidly/Controllers/HomeController.cs
@@ -3,17 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
 
 namespace Vidly.Controllers
 {
     [AllowAnonymous]
     public class HomeController : Controller
     {
+        //To enable cache on particular view/controller
+        [OutputCache(Duration = 50, Location = OutputCacheLocation.Server, VaryByParam = "genre")] 
         public ActionResult Index()
         {
             return View();
         }
 
+        //To disable caching
+        [OutputCache(Duration = 0, VaryByParam = "*", NoStore = true)]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
